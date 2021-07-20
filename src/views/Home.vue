52053d228b6b157cdfa2008b915c2b290cd7fc82
Jordan <jcharrison12@gmail.com>
2021-07-20 10:33:26 -0500

    Adds index and create functionality for movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+     <button v-on:click="createMovie()">Create New Movie</button>
+    <div v-for="movie in movies" :key="movie.id">
+      <h1>{{ movie.title }}</h1>
+      <h2>{{ movie.year }}</h2>
+      <p>{{ movie.plot }}</p>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: "Robocop",
+        year: 1987,
+        plot: "In a dystopic and crime-ridden Detroit, a terminally wounded cop returns to the force as a powerful cyborg haunted by submerged memories.",
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
