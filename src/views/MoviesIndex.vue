ec4024b76f381859a9841e3cfcc3fb1011eb1209
Jordan <jcharrison12@gmail.com>
2021-07-23 09:51:33 -0500

    Adds CRUD actions on separate views

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="movies-index">
+    <div v-for="movie in movies" :key="movie.id">
+      <router-link v-bind:to="`/movies/${movie.id}`">
+        <h1>{{ movie.title }}</h1>
+      </router-link>
+      <h2>{{ movie.year }}</h2>
+      <p>{{ movie.plot }}</p>
+    </div>
+  </div>
+</template>
+<style></style>
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.movies);
+      });
+    },
+  },
+};
+</script>
